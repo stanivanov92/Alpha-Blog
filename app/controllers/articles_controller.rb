091f6434acce67fbe02e2551b6077b39e845acd2
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-05-29 21:56:42 +0100

    Flash notice message embeded for new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-
+	  @article = Article.new
 	end
 
 	def create
-	@article = Article.new(params.require(:article).permit(:title, :description))
-	@article.save
-	redirect_to @article
+	  @article = Article.new(params.require(:article).permit(:title, :description))
+
+	  if @article.save
+	  	flash[:notice] = "Article was created successfully"
+	    redirect_to @article
+	  else
+	    render 'new'
+	  end
+
 	end
 end
