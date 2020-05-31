ae2dd6bd0f08301a5c34807821cdc783a9944403
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-06-01 00:08:34 +0100

    Refactoring controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
- 
+ 	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
 	def show
-	  @article = Article.find(params[:id])
+
 	end
 
 	def index
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-	  @article = Article.new(params.require(:article).permit(:title, :description))
+	  @article = Article.new(article_params)
 
 	  if @article.save
 	  	flash[:notice] = "Article was created successfully"
@@ -25,16 +26,35 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-	  @article = Article.find(params[:id])
+
 	end
 
 	def update
-		@article = Article.find(params[:id])
-	  if @article.update(params.require(:article).permit(:title, :description))
+	  if @article.update(article_params)
 		flash[:notice] = "Successfully updated article"
 		redirect_to @article
 	  else
 		render 'edit'
 	  end	
+	end
+
+	def destroy
+	  if @article.destroy
+	    flash[:notice] = "Item was successfully deleted"
+	  else
+ 	 	flash[:notice] = "The operation was Unseccessfull"
+	end
+	    redirect_to articles_path
+
+	end
+
+	private
+
+	def set_article
+		  @article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 end
