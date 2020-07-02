634da1b595506b61b118f3a6ddd683a876e5e216
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-07-02 13:06:35 +0100

    User's validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
+# Class Article
 class Article < ApplicationRecord
-
-validates :title, presence: true, length: {minimum: 5, maximum: 25}
-validates :description, presence: true, length: {minimum: 5, maximum: 105}
-
+  validates :title, presence: true, length: { minimum: 5, maximum: 25 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 105 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
+# Class User
 class User < ApplicationRecord
-  
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
