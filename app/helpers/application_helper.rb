5941cfdbdf2d44f73f793e596d81a1193440b8ec
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-07-12 13:12:20 +0100

    Show user controller and view plus belonging articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Application helper
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email
+    size = options[:size]
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name)
+  end
 end
