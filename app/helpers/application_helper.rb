714a7e3144156385dd79b67324af4ac0ddcc37e9
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-07-19 16:57:49 +0100

    Home page different for logged in and not logged users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,4 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.name)
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
 end
