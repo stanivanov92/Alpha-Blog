c5b6f82e7ac23a4e030d624841af9876de4ceb41
Stanislav Ivanov <stan_ivanov@icloud.com>
2020-07-26 22:48:41 +0100

    Addet tests for name presence and length

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@
 
 # Class Category
 class Category < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates_uniqueness_of :name
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,9 +3,31 @@
 require 'test_helper'
 # Class Category Test
 class CategoryTest < ActiveSupport::TestCase
-  test 'category should be valid' do
+  def setup
     @category = Category.new(name: 'Sports')
+  end
+
+  test 'category should be valid' do
     assert @category.valid?
-    assert @category.name == 'Sports', ['The name is not equal to sport']
+  end
+
+  test 'name should be preset' do
+    @category.name = ''
+    assert_not @category.valid?
+  end
+
+  test 'name should be unique' do
+    @category.save
+    @category2 = Category.new(name: 'Sports')
+    assert_not @category2.valid?
+  end
+
+  test 'name should not be too short' do
+    @category.name = 'a' * 2
+    assert_not @category.valid?
+  end
+  test 'name should not be too long' do
+    @category.name = 'a' * 26
+    assert_not @category.valid?
   end
 end
